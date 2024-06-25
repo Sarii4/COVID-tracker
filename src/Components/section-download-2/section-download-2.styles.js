7902d6a36f713d50528d7e0cc1489b994d860f47
CIBA <sanchezmariabelen01@gmail.com>
2024-06-25 03:01:55 +0200

    section download 2

---
+++ src/Components/section-download-2/section-download-2.styles.js
@@ -0,0 +1,123 @@
+import styled from "styled-components"
+
+
+
+export const TotalContainer = styled . div`
+display:flex;
+justify-content: space-around;
+background-color: #242674;
+padding-bottom:35px;
+
+`;
+export const ItemContainer = styled . div`
+display: flex;
+justify-content: space-between;
+
+`; 
+
+export const PhoneContainer = styled .div`
+display: flex;
+align-items: center;
+justify-content: center;
+margin-left:10%;
+margin-top:3%;
+
+`;
+export const ImagePhone = styled . img`
+width: 400px;
+height:350px;
+margin-left:-55px;
+
+
+
+`; 
+
+export const TextContainer = styled. div`
+display: flex;
+flex-direction: column;
+width: 50%;
+height:50%;
+margin-top:8%;
+padding:5px;
+
+
+`;
+export const Number = styled .h3`
+font-size:40px;
+margin:0;
+
+`;
+
+export const Title = styled .h1`
+font-size:35px;
+margin-bottom:0;
+`;
+
+
+export const Paragraph = styled .p`
+font-size:16px;
+margin-bottom:0;
+`;
+
+export const ListContainer = styled .div`
+display:flex;
+justify-content: space-between;
+background-color:#242674;
+padding-left:40px;
+padding-right:40px;
+
+`;
+
+export const BoxContainer = styled .div`
+display:flex;
+flex-direction:column;
+align-items:center;
+
+`;
+
+
+export const Button = styled.button`
+display: inline-block;
+padding: 20px 40px;
+background-color:#3639AE;
+font-weight: 600;
+font-size: 12px;
+text-transform: uppercase;
+letter-spacing: 1px;
+border: 1px solid transparent;
+width:65%;
+height:30%;
+margin-top:15px;   
+
+`;
+
+export const ImagePadlock = styled.img`
+width:30px;
+height: 30px;
+margin:0;
+
+`;
+
+export const ImageUp = styled.img`
+width:30px;
+height: 30px;
+`;
+
+export const ImageCrown = styled.img`
+width:40px;
+height: 40px;
+`;
+
+
+export const ImageHeart = styled.img`
+width:30px;
+height: 30px;
+`;
+
+export const LeyendNumber = styled.h2`
+font-size:12px;
+
+`;
+
+
+
