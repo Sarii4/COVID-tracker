83d231659a80eb8c42cc9f7f6740d755d1f6d088
castellanorn <castellanorn@gmail.com>
2024-06-27 15:11:04 +0200

    edit APP.jsx, add Layout.jsx

---
+++ src/Components/layout/Layout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+const Layout = () => {
+    return (
+        <>
+            <nav>SideBar</nav>
+            <aside>Aside</aside>
+            <main>Outlet</main>
+        </>
+    )
+}
+
+export default Layout
