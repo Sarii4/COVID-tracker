bfc8b67b5dbbe6678b7245579e824079fe5df9fd
ShailaGonzalez <shailagonzalezcarrero@gmail.com>
2024-06-29 16:48:26 +0200

    upload section

--- src/Components/section-download-1/Section-Download.style.js
+++ src/Components/section-download-1/Section-Download.style.js
@@ -7,27 +7,40 @@ import styled from 'styled-components';
 export const SectionTotalContainer = styled.div` 
 background-color: #254179 ;
 display: flex;
-flex-direction: row;
-width: 1024px;
-max-width: 1200px;
-height: 1366px;
+flex-direction: column;
+width: 100%;
+height: 100px;
 justify-content: space-between;
-align-items: flex-start;
-padding:30px;
+align-items: center;
+padding:20px;
 background: linear-gradient(to bottom, #4e90b6, #235e8d, #203e75, #20346d, #181251); 
+
+
+@media (min-width: 768px) {
+    flex-direction: row;
+    height: 844px;
+    padding: 30px;
+  }
+
+  @media (min-width: 1440px) {
+    height: 900px;
+  }
 
 `;
 
 export const SectionContainer = styled.div`
   display: flex;
   flex-direction: column;
-  width: 50%;
-  max-width : 1200px;
-  height: auto;
-  padding: 20px;
+  width: 60%;
   margin: 0 auto;
   align-items: flex-start;
   position: relative;
+
+  @media (min-width: 768px) {
+    width: 60%;
+    padding: 20px;
+    margin: 0 auto;
+  }
 
   `;
 
@@ -38,23 +51,31 @@ export const Title = styled.h1`
   letter-spacing: 1px;
   line-height: 1.1;
   text-align: left;
-  text-size-adjust: 100%;
-  width: 450px; 
-  height: 150px; 
   margin-bottom: 15px;
   color: #ffffff;
-  margin: 10px 0 0 30px;
+  margin: 10px 0px;
+
+  @media (min-width: 768px) {
+    font-size: 40px;
+    width: 450px; 
+    height: 150px;
+    margin: 10px 100px 0;
+  }
 
 `;
 
 export const Description = styled.p`
-  font-size: 13px;
-  margin-bottom: 20px;
+  font-size: 14px;
   text-align: left;
   color: #ffffff;
   line-height: 1.5;
-  max-width: 420px;
-  margin: 40px 0 0 30px;
+  line-height: 1.5;
+  margin: 20px 0;
+  
+  @media (min-width: 768px) {
+    margin: 40px 100px 0;
+    max-width: 420px;
+  }
   
 `;
 
@@ -63,110 +84,138 @@ export const StyledButton = styled.button`
   background-color: rgb(18, 17, 88); 
   border: 1px solid rgb(54, 57, 174); 
   border-radius: 30px;
-  margin: 30px 0 0 35px; 
   cursor: pointer; 
   outline: none; 
-  font-size: 12px;
+  font-size: 14px;
   font-weight: bold;
-  padding: 15px 50px; 
-  margin: 40px 0 0 40px;
+  padding: 15px 30px; 
+  margin: 20px 0 ;
   &:hover {
     border-color: rgb(18, 17, 88); 
     
   }
 
+
+  @media (min-width: 768px) {
+    padding: 15px 50px;
+    margin: 40px 120px 0;
+  }
   
 `;
 
 export const ImageContainer = styled.div`
-  width: 50%;
+  width: 100%;
   height: auto;
   display: flex; 
   align-items: center;
-  transform: translateY(30%);
-  left: 0;
-  margin-left: 30px;
+  justify-content: flex-start;
+  margin-top: 300px;
+ margin-left: 0;
+
+
+  @media (min-width: 768px) {
+    width: 50%;
+    justify-content: flex-end;
+    align-items: center;
+    margin-left: 30px;
+  }
 `;
 
 export const StyledImage = styled.img`
-  height: 422.91px;
-  width: 350px;
-  margin: 0 0 85px 0px;
-  float: right;
-  position: relative; 
+  height: 200px;
+  width: 100%;
+  margin: 0 ;
+  max-width: 350px;
   top: -120px;
+
+  @media (min-width: 768px) {
+    height: 422.91px;
+    width: 350px;
+    margin: 0 0 85px 0px;
+    position: relative;
+    top: -120px;
+  }
+
+
 `;
 
 export const SponsorContainer = styled.div`
-background-color: blue;
 display: flex;
 justify-content: center;
- position: absolute; 
-  bottom: 250px; 
-  width: 900px;
-  height: 80px; 
-  display: flex;
-  justify-content: space-around;
+  width: 100%;
+  height: 60px; 
   align-items: center;
   background-color: white;
   padding: 10px;
   margin-top: 20px;
   border-radius: 5px;
-  margin: 10px 0px 0px 45px;
+  flex-wrap: wrap;
+  
 
+
+  @media (min-width: 768px) {
+    position: absolute;
+    bottom: -1550px;
+    width: 900px;
+    height: 80px;
+    margin: 20px 60px -95px 120px;
+  }
 
 `;
 
-export const StyledSponsor0 = styled.img`
-max-height: 100%;
-object-fit: contain;
-margin-left: 10px; 
+const StyledSponsor = styled.img`
+  max-height: 100%;
+  object-fit: contain;
+  margin: 0 10px; 
+`;
+
+export const StyledSponsor0 = styled(StyledSponsor)`
+  margin-left: -400px;
 `;
 
 
-export const StyledSponsor1 = styled.img`
-max-height: 100%;
-object-fit: contain;
-margin-left: 10px; 
+export const StyledSponsor1 = styled(StyledSponsor)`
+   margin-left: -100px;
+`;
+
+export const StyledSponsor2 = styled(StyledSponsor)`
+   margin-left: -100px;
+
+`;
+
+export const StyledSponsor3 = styled(StyledSponsor)`
+  /* No se especifica margen izquierdo, usa el valor predeterminado de 10px */
 `;
 
 
-export const StyledSponsor2 = styled.img`
-max-height: 100%;
-object-fit: contain;
-margin-left: 10px; 
-`;
-
-export const StyledSponsor3 = styled.img`
-max-height: 100%;
-object-fit: contain;
-margin-left: 10px; 
+export const StyledSponsor5 = styled(StyledSponsor)`
+  /* No se especifica margen izquierdo, usa el valor predeterminado de 10px */
 `;
 
 
-export const StyledSponsor5 = styled.img`
-max-height: 100%;
-object-fit: contain;
-margin-left: 10px; 
-`;
-export const StyledSponsor4 = styled.img`
-max-height: 100%;
-object-fit: contain;
-margin-left: 10px; 
+export const StyledSponsor4 = styled(StyledSponsor)`
+  margin-left: -650px;
 `;
 
 
 export const ContainerButtons = styled.div` 
 display: flex;
-flex-direction: column;
-align-items: flex-start;
-position: absolute;
-bottom: 0;
-left: px;
-width: 200px;
-height: 450px; 
-padding-left: 0.5px;
+  flex-direction: row; 
+  justify-content: space-around;
+  width: 100%;
+  padding: 10px 0;
 
+
+  @media (min-width: 768px) {
+    flex-direction: column;
+    align-items: flex-start;
+    position: absolute;
+    bottom: -1555px;
+    left: 80px;
+    width: 200px;
+    height: 100px;
+    padding-left: 0.5px;
+  }
 `;
 
 export const StyledShare = styled.img`
