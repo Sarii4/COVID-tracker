8a4eb4328fb0b80496a463ecbf6c40524449ea64
sarii4 <sarita248@outlook.es>
2024-07-01 12:46:25 +0200

    responsive cards

--- src/Components/Carousel/Styled-Carousel.jsx
+++ src/Components/Carousel/Styled-Carousel.jsx
@@ -9,20 +9,26 @@ export const Card = styled.div`
     margin: 10px;
     padding: 15px;
     border-radius: 5px;
+    transition: transform 0.2s;
+    margin-bottom:20%
+    overflow: hidden;
+    &:hover {
+        transform: scale(1.05);
+    }
     & h1{
         margin:0;
         color: #333;
-        font-size:23px
+        font-size:120%
     }
     & p{
         color: #333;
-        font-size: 14px;
+        font-size: 90%;
     }
     & img{
         height: 55px;
     }
-`
-export const ContenedorCarousel = styled.div`
-display:flex;
-flex-direction:row;
+    @media (max-width: 600px) {
+        display:flex
+        flex-direction:column;
+        }
 `
